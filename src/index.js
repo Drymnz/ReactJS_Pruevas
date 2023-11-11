c95bc8b9fc4a0cd4b7371cc6a2df6e5fc0115a77
Benjamin de Jesus Perez Aguilar <mcdreck276@gmail.com>
2023-11-10 23:57:59 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+/* metodo de js
+const element = document.createElement('h1');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+element.innerText = 'hola mundo';
+
+const conteiner = document.getElementById('root');
+
+conteiner.appendChild(element); 
+*/
+
+import React from "react";
+import ReactDOM from 'react-dom'
+
+
+const user = {
+  firtName:'Benjamin',
+  lastName:'Perez',
+  avatar:'http'
+}
+
+function getName(user)
+{
+  return user.firtName + ' ' +user.lastName
+}
+
+function getGreeting(user) 
+{
+  if (user) {
+    return <h1>Hola {user.firtName}</h1>
+  } else {
+    return <h1>Quien sos ?</h1>
+  }  
+}
+
+const nombre = 'Benjamin'
+/* const element = <h1>Hola mundo {nombre}, {getName(user)} </h1>; */
+/* const element = <div>{getGreeting(user)}</div> */
+const element = <div>{getGreeting()}</div>
+
+const conteiner = document.getElementById('root');
+
+//ReactDOM.render(__que,__donde)
+ReactDOM.render(element,conteiner); 
